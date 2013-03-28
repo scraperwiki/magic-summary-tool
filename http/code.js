9f7010a5bef5efbb596d48aea07eb208683572ed
Francis Irving <francis@flourish.org>
2013-03-28 01:30:39 +0000

    Show most common value in larger tables

--- http/code.js
+++ http/code.js
@@ -1,6 +1,12 @@
+// TODO:
+// Make pie charts of the group data http://blog.stephenboak.com/2011/08/07/easy-as-a-pie.html
+// Detect type of columns: integer, float, option, free text, url, date/time, lat/lng
+// Show most common elements - if more than x% of total
+
 $(function() {
   var meta
   var table
+  var total
   var groups = {}
 
   var handle_error = function(err) {
@@ -9,20 +15,47 @@ $(function() {
 
   // for columns that have less than 10 values, show in a table
   var simple_groups = function(col, group) {
-    if (group.length < 10) {
-      var html = '<div class="item">'
-      html += '<table class="table table-striped">'
-      html += '<tr><th>' + col + '</th><th>count</th></tr>'
-      $.each(group, function(ix, value) {
-        html += '<tr>'
-        html += '<td>' + value.val + '</td>'
-        html += '<td>' + value.c + '</td>'
-        html += '</tr>'
-      })
-      html += '</table></div>'
-      console.log(col, html)
+    var html = '<div class="item">'
+
+    if (group.length == 1) {
+      html += '<p><b>' + col + '</b> is always <b>' + group[0].val + '</b></p>'
+      html += '</div>'
       $('#simple_groups').append(html)
-    }  
+      return
+    }
+
+    html += '<table class="table table-striped table-hover">'
+    html += '<tr><th>' + col + '</th><th>count</th></tr>'
+    var gotten = 0
+    $.each(group, function(ix, value) {
+      // for long lists, only show items accounting for 2% of rows
+      if (group.length > 10) {
+        if (value.c / total < 0.02) {
+          html += '<tr class="' + cls + '">'
+          html += '<td>&hellip;</td>'
+          html += '<td>&nbsp;</td>'
+          html += '</tr>'
+          return false
+        }
+      }
+
+      var cls = ''
+      if (value.val == null) {
+        cls = 'error'
+      }
+      html += '<tr class="' + cls + '">'
+      html += '<td>' + value.val + '</td>'
+      html += '<td>' + value.c + '</td>'
+      html += '</tr>'
+
+      gotten++
+    })
+    if (gotten == 0) {
+      return
+    }
+    html += '</table></div>'
+    console.log(col, html)
+    $('#simple_groups').append(html)
   }
 
   async.auto({
@@ -35,8 +68,15 @@ $(function() {
         cb()
       })
     },
+    // Get total number of rows
+    getTotal: [ 'getMeta', function(cb) {
+      scraperwiki.sql("select count(*) as c from " + table, function(data) {
+        total = data[0].c
+        cb()
+      }, handle_error)
+    } ],
     // For every column, count the number of meta groupings
-    getGroups: [ 'getMeta', function(cb1) {
+    getGroups: [ 'getTotal', 'getMeta', function(cb1) {
       async.forEach(meta.columnNames, function(col, cb2) {
         scraperwiki.sql("select " + col + " as val, count(*) as c from " + table + " group by " + col + " order by c desc", function(group) {
           groups[col] = group
