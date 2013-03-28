e3ee882d6caf2406df5fb0f39cf230a167d25917
Francis Irving <francis@flourish.org>
2013-03-28 01:06:59 +0000

    Looks kind of OK

--- http/code.js
+++ http/code.js
@@ -1,5 +1,4 @@
 $(function() {
-
   var meta
   var table
   var groups = {}
@@ -8,29 +7,53 @@ $(function() {
     console.log("err", err)
   }
 
+  var simple_groups = function(col, group) {
+    if (group.length < 10) {
+      var html = '<div class="item">'
+      html += '<h2>' + col + '</h2>'
+      html += '<table class="table table-striped"><tr><th>value</th><th>count</th></tr>'
+      $.each(group, function(ix, value) {
+        html += '<tr>'
+        html += '<td>' + value.val + '</td>'
+        html += '<td>' + value.c + '</td>'
+        html += '</tr>'
+      })
+      html += '</table></div>'
+      console.log(col, html)
+      $('#simple_groups').append(html)
+    }  
+  }
+
   async.auto({
     // Get schema of SQL database
     getMeta: function(cb) {
       scraperwiki.sql.meta(function(lmeta) {
-        meta = lmeta
         table = "tweets" // XXX todo dehardcode this
+        meta = lmeta['table'][table]
         console.log("meta", meta)
         cb()
       })
     },
-    getGroups: [ 'getMeta', function(cb) {
-      async.forEach(meta['table'][table].columnNames, function(col, cb) {
-        scraperwiki.sql("select " + col + ", count(*) as c from " + table + " group by " + col + " order by c desc", function(data) {
-          console.log("getGroups", col, data)
-          groups[col] = data
-          cb()
+    // For every column, count the number of meta groupings
+    getGroups: [ 'getMeta', function(cb1) {
+      async.forEach(meta.columnNames, function(col, cb2) {
+        scraperwiki.sql("select " + col + " as val, count(*) as c from " + table + " group by " + col + " order by c desc", function(group) {
+          groups[col] = group
+          simple_groups(col, group)
+          cb2()
         }, handle_error)
       }, function() {
-        // done getting grouping
+        cb1()
       })
     } ]
   }, function() {
     console.log("async.auto done")
   })
+
+  $('#simple_groups').masonry({
+    // options
+    itemSelector : '.item',
+    columnWidth : 240
+  });
 
 })
