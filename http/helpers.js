62acd3789681af2ed5df785f1e0feec6fe13c360
Francis Irving <francis@flourish.org>
2013-04-15 11:03:57 +0000

    Fix comma adding with decimal points

--- http/helpers.js
+++ http/helpers.js
@@ -50,5 +50,7 @@ var numberise = function(val) {
 
 // http://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
 var add_commas = function(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    var parts = x.toString().split(".")
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+    return parts.join(".")
 }
